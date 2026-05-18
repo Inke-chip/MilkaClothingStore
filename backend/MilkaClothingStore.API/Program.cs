04bbe8ba99f837cc8ca0c66bccfb5d3a3b069c75
Inke-chip <Littlechipink@gmail.com>
2026-05-19 00:43:02 +0300

    fix(бэкенд): очистить Program.cs от конфликтующих зависимостей Swagger и настроить чистый OpenAPI

--- backend/MilkaClothingStore.API/Program.cs
+++ backend/MilkaClothingStore.API/Program.cs
@@ -1,18 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using MilkaClothingStore.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<StoreDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 var summaries = new[]
 {
@@ -21,7 +30,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -35,6 +44,7 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
+// Запись для тестового маршрута
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
