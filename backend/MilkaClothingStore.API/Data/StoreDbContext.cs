4b63af1b1458f4468120702e22d1ef7ce22dd8cf
Inke-chip <Littlechipink@gmail.com>
2026-05-19 11:01:19 +0300

    feat(база-данных): зарегистрировать Products и ProductVariants в StoreDbContext

--- backend/MilkaClothingStore.API/Data/StoreDbContext.cs
+++ backend/MilkaClothingStore.API/Data/StoreDbContext.cs
@@ -17,6 +17,9 @@ namespace MilkaClothingStore.API.Data
         public DbSet<Color> Colors { get; set; }
         public DbSet<Size> Sizes { get; set; }
 
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductVariant> ProductVariants { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
