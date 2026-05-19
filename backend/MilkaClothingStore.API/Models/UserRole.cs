76bd0daec5963bdbf93d8216c7e7201381ec7836
Inke-chip <Littlechipink@gmail.com>
2026-05-19 10:27:02 +0300

    feat(аутентификация): успешно настроить систему RBAC для многих ролей и проверить сквозной сценарий Login

--- backend/MilkaClothingStore.API/Models/UserRole.cs
+++ backend/MilkaClothingStore.API/Models/UserRole.cs
@@ -2,13 +2,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MilkaClothingStore.API.Models
 {
-    [Table("UserRoles")] // Указываем точное имя промежуточной таблицы
+    [Table("UserRoles")] 
     public class UserRole
     {
         public int UserId { get; set; }
         public int RoleId { get; set; }
 
-        // Навигационные свойства для удобства построения запросов
         [ForeignKey("UserId")]
         public User? User { get; set; }
 
